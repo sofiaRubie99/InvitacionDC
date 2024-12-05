21e49666ee65a8fb5a717b704e7202705ff26307
sofiaRubie <81442319+SRG-28@users.noreply.github.com>
2024-12-05 16:58:39 -0600

    Final Invitation

---
+++ js/countdown.js
@@ -0,0 +1,27 @@
+function startCountdown() {
+    // Fecha de la boda s
+    const weddingDate = new Date('2025-01-18T16:00:00-06:00').getTime(); // Hora en UTC-6 (Costa Rica)
+
+    setInterval(function() {
+        // Fecha y hora actual ajustada automáticamente a la zona horaria local 
+        const now = new Date().getTime();
+        const timeLeft = weddingDate - now;
+
+        const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+        document.getElementById('days').innerText = days;
+        document.getElementById('hours').innerText = hours;
+        document.getElementById('minutes').innerText = minutes;
+        document.getElementById('seconds').innerText = seconds;
+
+        if (timeLeft < 0) {
+            clearInterval();
+            document.getElementById('countdown').innerHTML = "<p>¡Es el gran día!</p>";
+        }
+    }, 1000);
+}
+
+window.onload = startCountdown;
